b568e040f2b70d9b29684bb9cfa1aa30fba59deb
kishore904 <kishorekumarkishorekumar7381@gmail.com>
2026-03-09 14:33:04 +0530

    UC2: Add room types using abstraction and inheritance

--- src/HotelBookingApp.java
+++ src/HotelBookingApp.java
@@ -1,25 +1,88 @@
+// -------------------------------------------------------
+// Abstract Room Class
+// -------------------------------------------------------
+abstract class Room {
 
+    protected String roomType;
+    protected int beds;
+    protected double price;
 
-public classHotelBookingApp {
+    public Room(String roomType, int beds, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Room Type : " + roomType);
+        System.out.println("Beds      : " + beds);
+        System.out.println("Price     : $" + price);
+    }
+}
+
+// -------------------------------------------------------
+// Single Room Class
+// -------------------------------------------------------
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super("Single Room", 1, 80.0);
+    }
+}
+
+// -------------------------------------------------------
+// Double Room Class
+// -------------------------------------------------------
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super("Double Room", 2, 140.0);
+    }
+}
+
+// -------------------------------------------------------
+// Suite Room Class
+// -------------------------------------------------------
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super("Suite Room", 3, 300.0);
+    }
+}
+
+// -------------------------------------------------------
+// Application Entry Point
+// -------------------------------------------------------
+public class HotelBookingApp {
 
     public static void main(String[] args) {
 
-        // Application header
-        System.out.println("\n******** HOTEL BOOKING MANAGEMENT SYSTEM ********");
+        System.out.println("=====================================");
+        System.out.println(" HOTEL BOOKING MANAGEMENT SYSTEM");
+        System.out.println(" Version 2.1 - Room Initialization");
+        System.out.println("=====================================\n");
 
-        // Application details
-        System.out.println("Application Version : v1.0");
-        System.out.println("Status              : System Starting...");
+        Room singleRoom = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suiteRoom = new SuiteRoom();
 
-        // Welcome message
-        System.out.println("\nWelcome User!");
-        System.out.println("The Hotel Booking System is now running.");
+        int singleRoomAvailable = 10;
+        int doubleRoomAvailable = 6;
+        int suiteRoomAvailable = 3;
 
-        // Application ready message
-        System.out.println("\nInitialization complete.");
-        System.out.println("System ready to manage hotel bookings.");
+        System.out.println("Single Room Details:");
+        singleRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + singleRoomAvailable);
+        System.out.println("--------------------------------");
 
-        // End of startup
-        System.out.println("\n***********************************************");
+        System.out.println("Double Room Details:");
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + doubleRoomAvailable);
+        System.out.println("--------------------------------");
+
+        System.out.println("Suite Room Details:");
+        suiteRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + suiteRoomAvailable);
+        System.out.println("--------------------------------");
     }
 }
